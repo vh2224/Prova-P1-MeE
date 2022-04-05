a21515030b637fdef464081e697ce2ade149f0d2
Vinicius <vh2224@github.com>
2022-04-05 20:01:32 -0300

    Feat: Renomeando variaveis para nome mais semantico utilizando Rename Variable

--- tennis.py
+++ tennis.py
@@ -15,60 +15,60 @@ class Game:
     def score(self):
         result = ""
         if (self.player1Points == self.player2Points and self.player1Points < 3):
-            if (self.player1Points==0):
+            if (self.player1Points == 0):
                 result = "Love-All"
-            if (self.player1Points==1):
+            if (self.player1Points == 1):
                 result = "Fifteen-All"
-            if (self.player1Points==2):
+            if (self.player1Points == 2):
                 result = "Thirty-All"
                 
-        if (self.player1Points==self.player2Points and self.player1Points>2):
+        if (self.player1Points == self.player2Points and self.player1Points > 2):
             result = "Deuce"
         
-        P1res = ""
-        P2res = ""
-        if (self.player1Points > 0 and self.player2Points==0):
-            if (self.player1Points==1):
-                P1res = "Fifteen"
-            if (self.player1Points==2):
-                P1res = "Thirty"
-            if (self.player1Points==3):
-                P1res = "Forty"
+        player_1_result = ""
+        player_2_result = ""
+        if (self.player1Points > 0 and self.player2Points == 0):
+            if (self.player1Points == 1):
+                player_1_result = "Fifteen"
+            if (self.player1Points == 2):
+                player_1_result = "Thirty"
+            if (self.player1Points == 3):
+                player_1_result = "Forty"
             
-            P2res = "Love"
-            result = P1res + "-" + P2res
-        if (self.player2Points > 0 and self.player1Points==0):
-            if (self.player2Points==1):
-                P2res = "Fifteen"
-            if (self.player2Points==2):
-                P2res = "Thirty"
-            if (self.player2Points==3):
-                P2res = "Forty"
+            player_2_result = "Love"
+            result = player_1_result + "-" + player_2_result
+        if (self.player2Points > 0 and self.player1Points == 0):
+            if (self.player2Points == 1):
+                player_2_result = "Fifteen"
+            if (self.player2Points == 2):
+                player_2_result = "Thirty"
+            if (self.player2Points == 3):
+                player_2_result = "Forty"
             
-            P1res = "Love"
-            result = P1res + "-" + P2res
+            player_1_result = "Love"
+            result = player_1_result + "-" + player_2_result
         
         
-        if (self.player1Points>self.player2Points and self.player1Points < 4):
-            if (self.player1Points==2):
-                P1res="Thirty"
-            if (self.player1Points==3):
-                P1res="Forty"
-            if (self.player2Points==1):
-                P2res="Fifteen"
-            if (self.player2Points==2):
-                P2res="Thirty"
-            result = P1res + "-" + P2res
-        if (self.player2Points>self.player1Points and self.player2Points < 4):
-            if (self.player2Points==2):
-                P2res="Thirty"
-            if (self.player2Points==3):
-                P2res="Forty"
-            if (self.player1Points==1):
-                P1res="Fifteen"
-            if (self.player1Points==2):
-                P1res="Thirty"
-            result = P1res + "-" + P2res
+        if (self.player1Points > self.player2Points and self.player1Points < 4):
+            if (self.player1Points == 2):
+                player_1_result="Thirty"
+            if (self.player1Points == 3):
+                player_1_result="Forty"
+            if (self.player2Points == 1):
+                player_2_result="Fifteen"
+            if (self.player2Points == 2):
+                player_2_result="Thirty"
+            result = player_1_result + "-" + player_2_result
+        if (self.player2Points > self.player1Points and self.player2Points < 4):
+            if (self.player2Points == 2):
+                player_2_result="Thirty"
+            if (self.player2Points == 3):
+                player_2_result="Forty"
+            if (self.player1Points == 1):
+                player_1_result="Fifteen"
+            if (self.player1Points == 2):
+                player_1_result="Thirty"
+            result = player_1_result + "-" + player_2_result
         
         if (self.player1Points > self.player2Points and self.player2Points >= 3):
             result = "Advantage " + self.player1Name
@@ -76,9 +76,9 @@ class Game:
         if (self.player2Points > self.player1Points and self.player1Points >= 3):
             result = "Advantage " + self.player2Name
         
-        if (self.player1Points>=4 and self.player2Points>=0 and (self.player1Points-self.player2Points)>=2):
+        if (self.player1Points >= 4 and self.player2Points >= 0 and (self.player1Points - self.player2Points) >=2 ):
             result = "Win for " + self.player1Name
-        if (self.player2Points>=4 and self.player1Points>=0 and (self.player2Points-self.player1Points)>=2):
+        if (self.player2Points >= 4 and self.player1Points >= 0 and (self.player2Points - self.player1Points) >=2 ):
             result = "Win for " + self.player2Name
         return result
     
@@ -91,8 +91,8 @@ class Game:
             self.P2Score()
     
     def P1Score(self):
-        self.player1Points +=1
+        self.player1Points += 1
     
     
     def P2Score(self):
-        self.player2Points +=1
+        self.player2Points += 1
